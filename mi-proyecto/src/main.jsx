c93ad7c8c557c9c87d42da3c4513dae18ee97348
SebasCadena <juan.cadena02@uceva.edu.co>
2025-03-30 22:18:11 -0500

    Instalación completa
    Instalación de firebase, tailwindcss y otros

---
+++ mi-proyecto/src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-2xl font-bold">
+          ¡Tailwind está funcionando! 🚀
+        </div>
+      );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+
+
+
+
+
+
+
