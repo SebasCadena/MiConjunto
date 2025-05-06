3abbbf66a8aa83e8cdc7135351900e45e91c8902
juan.cadena02 <juan.cadena02@uceva.edu.co>
2025-05-06 14:08:13 +0000

    Primer intento de menu desplegable

--- mi-proyecto/src/pages/PanelInquilino.jsx
+++ mi-proyecto/src/pages/PanelInquilino.jsx
@@ -15,6 +15,7 @@ const PanelInquilino = ({ userId }) => {
   const [loading, setLoading] = useState(true); // Estado para indicar si la información está cargando
   const [hayPagosPendientes, setHayPagosPendientes] = useState(false);
   const [cantidadPagosPendientes, setCantidadPagosPendientes] = useState(0);
+  const [showNotificationsMenu, setShowNotificationsMenu] = useState(false);
   
   useEffect(() => {
     const cargarDatosUsuario = async () => {
@@ -96,6 +97,39 @@ const PanelInquilino = ({ userId }) => {
 
             <p className="text-gray-600">{usuario?.nombre || "Nombre no disponible"}</p>
           </div>
+          {/* Botón de Notificaciones */}
+          <div className="relative w-full">
+           <button
+              onClick={() => setShowNotificationsMenu(!showNotificationsMenu)}
+              className="mb-4 bg-blue-500 rounded-full p-2 relative"
+            >
+                <svg className="w-5 h-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20">
+                    <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z"/>
+                </svg>
+                <div className="absolute block w-3 h-3 bg-red-500 border-2 border-white rounded-full -top-0.5 start-2.5 dark:border-gray-900"></div>
+              </button>
+              {showNotificationsMenu && (
+                  <div className="z-20 absolute top-10 right-0 w-[300px] bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-800 dark:divide-gray-700">
+                      <div className="block px-4 py-2 font-medium text-center text-gray-700 rounded-t-lg bg-gray-50 dark:bg-gray-800 dark:text-white">
+                          Notifications
+                      </div>
+                      <div className="divide-y divide-gray-100 dark:divide-gray-700">
+                          {/* Ejemplo de un elemento de la lista */}
+                          <a href="#" className="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
+                              {/* Espacio para la imagen o icono */}
+                              <div className="w-full ps-3">
+                                  {/* Contenido de la notificación */}
+                              </div>
+                          </a>
+                      </div>
+                      <a href="#" className="block py-2 text-sm font-medium text-center text-gray-900 rounded-b-lg bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white">
+                          <div className="inline-flex items-center ">
+                              Ver todos
+                          </div>
+                      </a>
+                  </div>
+              )}
+          </div>
           <nav className="flex flex-col space-y-4 w-full">
             <button
               onClick={() => setView("contrato")}
@@ -133,6 +167,9 @@ const PanelInquilino = ({ userId }) => {
       {/* Contenido principal */}
       <main className="flex-1 p-8">
         {view === "contrato" && <Contrato userId={userId} />}        
+        
+         
+        
          {view === "editarPerfil" && <EditarPerfil userId={userId} />}
         {view === "pagos" && <Pagos userId={userId} />}
         {view === "notificaciones" && <Notificaciones userId={userId} />}
