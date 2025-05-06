ca623eb3bbab218c5775c3664a079e268f9c284a
Maria Jose <maria.ramirez11@uceva.edu.co>
2025-05-06 15:57:03 -0500

    Base para aside

--- mi-proyecto/src/pages/PanelInquilino.jsx
+++ mi-proyecto/src/pages/PanelInquilino.jsx
@@ -1,35 +1,34 @@
 import { useState, useEffect } from "react";
 import { db, auth } from "./firebaseConfig";
-import { doc, getDoc, collection, query, where, getDocs } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { getContratos } from "../utils";
-import { signOut } from "firebase/auth"; // Asegúrate de importar signOut
-import Contrato from "./vistasInquilino/contrato"; // Importamos el componente Contrato
-import EditarPerfil from "./vistasInquilino/editarPerfil"; // Importamos el componente EditarPerfil
+import { signOut } from "firebase/auth";
+import Contrato from "./vistasInquilino/contrato";
+import EditarPerfil from "./vistasInquilino/editarPerfil";
 import Pagos from "./vistasInquilino/pagos";
-import { useNavigate } from "react-router-dom";// Importar useNavigate
+import { useNavigate } from "react-router-dom";
 
 const PanelInquilino = ({ userId }) => {
   const [usuario, setUsuario] = useState(null);
-  const [view, setView] = useState("contrato"); // Estado para controlar la vista actual
-  const [loading, setLoading] = useState(true); // Estado para indicar si la información está cargando
+  const [view, setView] = useState("contrato");
+  const [loading, setLoading] = useState(true);
   const [hayPagosPendientes, setHayPagosPendientes] = useState(false);
   const [cantidadPagosPendientes, setCantidadPagosPendientes] = useState(0);
-  
+
   useEffect(() => {
     const cargarDatosUsuario = async () => {
       setLoading(true);
       try {
         const contratosData = await getContratos(userId);
-        if (contratosData.length <= 0) return
+        if (contratosData.length <= 0) return;
         const contratoData = contratosData[0];
         const fechaActual = new Date();
-        const mesActual = fechaActual.getMonth() + 1; // Sumamos 1 porque los meses van de 0 a 11
+        const mesActual = fechaActual.getMonth() + 1;
         const añoActual = fechaActual.getFullYear();
         const cobrosPendientes = contratoData.cobros.filter((cobro) => {
           const fechaVencimiento = cobro.fecha_vencimiento.toDate();
           const mesVencimiento = fechaVencimiento.getMonth() + 1;
           const añoVencimiento = fechaVencimiento.getFullYear();
-          // Verificar si el cobro está vencido y no ha sido pagado, o si es el mes actual y no ha sido pagado
           return (
             ((fechaVencimiento < fechaActual && cobro.estado !== "Pagado")) ||
             (mesVencimiento === mesActual &&
@@ -38,97 +37,153 @@ const PanelInquilino = ({ userId }) => {
           );
         });
         setCantidadPagosPendientes(cobrosPendientes.length);
-        setHayPagosPendientes(cobrosPendientes.length > 0)
+        setHayPagosPendientes(cobrosPendientes.length > 0);
         const usuarioSnap = await getDoc(doc(db, "users", userId));
 
         if (usuarioSnap.exists()) {
           setUsuario(usuarioSnap.data());
         } else {
-          console.error("No se encontro el documento del usuario.");
+          console.error("No se encontró el documento del usuario.");
         }
-        } catch (error) {
-          console.error("Error al cargar los datos del usuario:", error);
-        } finally{
-          setLoading(false);
-        }
+      } catch (error) {
+        console.error("Error al cargar los datos del usuario:", error);
+      } finally {
+        setLoading(false);
+      }
     };
     if (userId) {
       cargarDatosUsuario();
     }
   }, [userId]);
-    const navigate = useNavigate();
+
+  const navigate = useNavigate();
   const cerrarSesion = async () => {
     try {
       await signOut(auth);
       alert("Sesión cerrada correctamente.");
       window.location.href = "/";
     } catch (error) {
-        console.error("Error al cerrar sesión:", error);
-        alert("Hubo un error al cerrar la sesión.");
+      console.error("Error al cerrar sesión:", error);
+      alert("Hubo un error al cerrar la sesión.");
     }
   };
-  
+
   if (loading) {
     return <p>Cargando información del usuario...</p>;
   }
-  return (
-    <div className="flex h-screen bg-teal-50">
-      {/* Barra lateral */}
-      <aside className="w-1/4 bg-teal-100 p-6 flex flex-col items-center">
-        <div className="mb-6 text-center">
-          <div className="w-24 h-24 bg-blue-500 rounded-full mb-4 mx-auto"></div>
-          <h2 className="font-bold text-lg">Mi Conjunto</h2>
-          {/* Mostrar mensaje de cobros pendientes */}
-          {hayPagosPendientes ? (
-            <div className="text-center">
-             <p className="text-red-600 font-semibold mb-2">
-                Tienes {cantidadPagosPendientes}
-                {cantidadPagosPendientes === 1 ? " pago" : " pagos"} pendiente
-                {cantidadPagosPendientes === 1 ? "" : "s"}.
-             </p>
-            </div>
-          ) : (
-            <div className="text-center">
-              <p className="text-green-600 font-semibold mb-2">No tienes pagos pendientes.</p>
-            </div>
-          )}
 
-            <p className="text-gray-600">{usuario?.nombre || "Nombre no disponible"}</p>
+  return (
+    <div>
+      {/* Barra de navegación superior */}
+      <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-start">
+              <button
+                data-drawer-target="logo-sidebar"
+                data-drawer-toggle="logo-sidebar"
+                aria-controls="logo-sidebar"
+                type="button"
+                className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              >
+                <span className="sr-only">Open sidebar</span>
+                <svg
+                  className="w-6 h-6"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                  ></path>
+                </svg>
+              </button>
+              <a href="#" className="flex ml-2 md:mr-24">
+                <img
+                  src="https://flowbite.com/docs/images/logo.svg"
+                  className="h-8 mr-3"
+                  alt="Logo"
+                />
+                <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
+                  Mi Conjunto
+                </span>
+              </a>
+            </div>
+            <div className="flex items-center">
+              <div className="flex items-center ml-3">
+                <button
+                  type="button"
+                  className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                  aria-expanded="false"
+                >
+                  <span className="sr-only">Open user menu</span>
+                  <img
+                    className="w-8 h-8 rounded-full"
+                    src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                    alt="user photo"
+                  />
+                </button>
+              </div>
+            </div>
           </div>
-          <nav className="flex flex-col space-y-4 w-full">
-            <button
-              onClick={() => setView("contrato")}
-              className="py-2 px-4 bg-blue-100 rounded hover:bg-blue-200"
-            >
-              Contrato
-            </button>
-            <button
-              onClick={() => setView("editarPerfil")}
-              className="py-2 px-4 bg-blue-100 rounded hover:bg-blue-200"
-            >
-              Editar Perfil
-            </button>
-            <button
-              onClick={() => setView("pagos")}
-              className="py-2 px-4 bg-blue-100 rounded hover:bg-blue-200"
-            >
-              Pagos
-            </button>
-            <button
-              onClick={cerrarSesion}
-              className="py-2 px-4 bg-red-500 text-white rounded hover:bg-red-600"
-            >
-              Cerrar sesión
-            </button>
-          </nav>
-        </aside>
-        
+        </div>
+      </nav>
+
+      {/* Menú lateral */}
+      <aside
+        id="logo-sidebar"
+        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+          <ul className="space-y-2 font-medium">
+            <li>
+              <button
+                onClick={() => setView("contrato")}
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                Contrato
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => setView("editarPerfil")}
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                Editar Perfil
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => setView("pagos")}
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                Pagos
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={cerrarSesion}
+                className="flex items-center p-2 text-red-500 rounded-lg hover:bg-red-100 dark:hover:bg-red-700 group"
+              >
+                Cerrar sesión
+              </button>
+            </li>
+          </ul>
+        </div>
+      </aside>
+
       {/* Contenido principal */}
-      <main className="flex-1 p-8">
-        {view === "contrato" && <Contrato userId={userId} />}        
-         {view === "editarPerfil" && <EditarPerfil userId={userId} />}
-        {view === "pagos" && <Pagos userId={userId} />} {/* Renderizar Pagos */}
-      </main>
+      <div className="p-4 sm:ml-64">
+        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+          {view === "contrato" && <Contrato userId={userId} />}
+          {view === "editarPerfil" && <EditarPerfil userId={userId} />}
+          {view === "pagos" && <Pagos userId={userId} />}
+        </div>
+      </div>
     </div>
   );
 };
